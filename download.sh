a3a22a6357700277f612782624233cda1500132a
Meshal <meshal.a@gmx.de>
2021-02-11 15:06:17 +0100

    Update and clean Code for revised Figures

---
+++ download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# downloads 9Gb dataset files
+wget https://drive.google.com/uc?export=download&id=13vf6Fcy6cCJUuGvbnj5sQDhayLRq7op1 -O mayr_et_al.tar.gz
+mkdir data; cd data
+gunzip ../mayr_et_al.tar.gz
+tar -xvf ../mayr_et_al.tar
